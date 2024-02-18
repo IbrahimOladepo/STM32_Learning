d17ea75ad2dc06e91041bad9c2c30fd903d0bc59
IbrahimOladepo <ibrahim.oladepo@yahoo.com>
2024-02-18 11:39:07 -0600

    Initial commit for HAL blinking code

---
+++ HAL_Blink/build/stm32f4xx_hal_rcc_ex.d
@@ -0,0 +1,52 @@
+build/stm32f4xx_hal_rcc_ex.o: \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ Inc/stm32f4xx_hal_conf.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Include/core_cm4.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+Inc/stm32f4xx_hal_conf.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Include/core_cm4.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Include/cmsis_version.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Include/cmsis_compiler.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Include/cmsis_gcc.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Include/mpu_armv7.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+/home/ibrahim/Embedded/STM32F401RE-Nucleo/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
--- LED_Blinking/main.c
+++ LED_Blinking/main.c
@@ -8,7 +8,57 @@
   Also make sure to have the right settings in system_stm32f4xx.c for input clock.
 */
 
-#include "stm32f4xx.h"
+// #include "stm32f401xe.h"
+#include "stm32f4xx_ll_gpio.h"
+#include "stm32f4xx_ll_rcc.h"
+
+
+// #define   STM32F401xE
+
+
+/* Private function prototypes -----------------------------------------------*/
+void     delay(void);
+void     SystemClock_Config(void);
+void     GPIO_ToggleBits(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin);
+
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+
+int main(void)
+{
+  /* Configure the system clock to 84 MHz */
+  SystemClock_Config();
+
+  // Configure the GPIO pin
+  LL_GPIO_InitTypeDef GPIO_InitStruct;
+  GPIO_InitStruct.Pin        = LL_GPIO_PIN_5;
+  GPIO_InitStruct.Mode       = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed      = LL_GPIO_SPEED_FREQ_MEDIUM;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull       = LL_GPIO_PULL_NO;
+
+  // Initialize the GPIO peripheral
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  while(1)
+  {
+    
+    // Toggle LED
+    GPIO_ToggleBits(GPIOA, LL_GPIO_PIN_5);
+
+    // Add some delay
+    delay();
+
+  }
+
+  return 0;
+}
 
 
 // Delay function
@@ -18,32 +68,72 @@ void delay(void)
 }
 
 
-int main(void)
+/* ==============   BOARD SPECIFIC CONFIGURATION CODE BEGIN    ============== */
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 84000000
+  *            HCLK(Hz)                       = 84000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 8
+  *            PLL_N                          = 336
+  *            PLL_P                          = 4
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale2 mode
+  *            Flash Latency(WS)              = 2
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
 {
-  // Setup RCC peripheral clock for the LED pin's port
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-
-  // Configure the GPIO pin
-  GPIO_InitTypeDef GPIO_InitStruct;
-  GPIO_InitStruct.GPIO_Pin  = GPIO_Pin_5;
-  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_25MHz;
-  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
-
-  // Initialize the GPIO peripheral
-  GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  while(1)
+  /* Enable HSE oscillator */
+  LL_RCC_HSE_EnableBypass();
+  LL_RCC_HSE_Enable();
+  while(LL_RCC_HSE_IsReady() != 1)
   {
-    
-    // Toggle LED
-    GPIO_ToggleBits(GPIOA, GPIO_Pin_5);
+  };
 
-    // Add some delay
-    delay();
+  /* Set FLASH latency */
+  // LL_FLASH_SetLatency(LL_FLASH_LATENCY_2);
 
-  }
+  /* Main PLL configuration and activation */
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_8, 336, LL_RCC_PLLP_DIV_4);
+  LL_RCC_PLL_Enable();
+  while(LL_RCC_PLL_IsReady() != 1)
+  {
+  };
 
-  return 0;
+  /* Sysclk activation on the main PLL */
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+  };
+
+  /* Set APB1 & APB2 prescaler */
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_2);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+
+  /* Set systick to 1ms */
+  SysTick_Config(84000000 / 1000);
+
+  /* Update CMSIS variable (which can be updated also through SystemCoreClockUpdate function) */
+  SystemCoreClock = 84000000;
+}
+
+
+/**
+  * @brief  Toggles the specified GPIO pins..
+  * @param  GPIOx: where x can be (A..I) to select the GPIO peripheral.
+  * @param  GPIO_Pin: Specifies the pins to be toggled.
+  * @retval None
+  */
+void GPIO_ToggleBits(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+{
+  GPIOx->ODR ^= GPIO_Pin;
 }
